440e5418628c8121ba74b8d02fdd7f3b0c3b8d0a
Rosaniline <xup6qup3@gmail.com>
2015-03-17 14:37:00 +0800

    Raw layout

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -9,5 +9,30 @@ $(document).ready(function(){
 
 	});
 
+	$(window).scroll(function(){
+
+		if ( $(document).scrollTop() > 150 ) {
+
+			console.log($('#img_logo').css('width'));	
+
+			$('#img_logo').css('width', '50%');
+
+			$('.navbar-nav > li > a').css({
+				'padding-top': '25px',
+				'padding-bottom': '25px',
+				'padding-right': '30px'
+			});
+		}
+
+
+		else {
+
+			$('#img_logo').css('width', '80%');	
+		}
+
+		
+
+	});
+
 
 });
